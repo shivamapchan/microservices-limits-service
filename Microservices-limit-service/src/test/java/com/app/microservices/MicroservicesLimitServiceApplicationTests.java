521926d601c3f5df202cbdb6e11852e7079fda35
Shiva Mapchan <shivamapchan@gmail.com>
2023-08-29 21:49:58 -0400

    Microservices-limits-service first commit

---
+++ Microservices-limit-service/src/test/java/com/app/microservices/MicroservicesLimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesLimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
